1596c6b6e92cb195890cb1a3c0440c9da969d728
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-04 13:12:50 +0530

    Add files via upload

---
+++ main1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "main1.h"
+using namespace std;
+
+int main(){
+    //variable define
+    // int age = 98;
+    // cout<< "My age is " << age << endl;
+    
+    //declaration  
+    // int age;
+    // cout << age;
+
+    //int age = 21;
+    //cout<< age<< endl;
+
+    //manupilation or updation 
+    // age = 101;
+    // cout<<age<<endl;  
+
+    int count = 5;
+
+    float share = 3.14;
+
+    char alphabet = 'z';
+
+    double weight = 55.69887;
+    
+    bool ismale = true;
+    bool ischild = 1;
+    bool isfemale = false;
+    bool isgirl = 0;
+
+    cout<<count<<endl;
+    cout<< share<<endl;
+    cout<<alphabet<<endl;
+    cout<<weight<<endl;
+    cout<<ismale<<endl;
+    return 0;
+}
