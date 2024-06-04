c63ff8f580303ba28c77523a224d66827834c34d
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-04 21:55:56 +0530

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float marks;
+    cout<< "Enter your marks" << endl; 
+    cin >> marks;
+    cout << "Sakshi's marks is:" << marks <<endl;
+    return 0;
+}
